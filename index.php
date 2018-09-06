929fae4d0b5738baa69b0b52d8d012b1afbf8f09
Szymon Waliczek <waliczek.szymon@gmail.com>
2018-09-06 10:20:46 +0100

    Adding base infrastructure to the project

--- index.php
+++ index.php
@@ -6,6 +6,20 @@ session_start();
 <html>
 
 <title>Transport Management System - 2018</title>
+<link rel="stylesheet" href="css/index.css" type="text/css">
+
+<head>
+
+</head>
+
+
+<body>
+	<div class="main_div">
+
+	</div>	
+
+</body>
+
 
 
 </html>
